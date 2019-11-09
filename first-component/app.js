22f7631ce9dfca845a254b759bcca118dd56b700
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-11-08 23:50:15 -0600

    Initial commit for first-component. There is still more to render the squares.

--- first-component/app.js
+++ first-component/app.js
@@ -1,5 +1,16 @@
 let FirstComponent = () => {
-    return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    return <div class="container d-flex w-50 h-50 justify-content-center">
+             <div class="left h-50">
+               <div class="red">RED</div>
+               <div class="blue">BLUE</div>
+             </div>
+             <div class="right h-50">
+               <div class="green">GREEN</div>
+               <div class="yellow">YELLOW</div>
+             </div>
+           </div>
+    
+    // <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
 }
 
 ReactDOM.render(<FirstComponent />, document.getElementById('root'))
