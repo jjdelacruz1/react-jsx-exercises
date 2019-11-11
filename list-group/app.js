6ead29989cd2f9289d2927b796a4969d957afcf7
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-11-11 12:19:55 -0600

    Finish the list-group exercise rendering the items a list group with bootstrap.

--- list-group/app.js
+++ list-group/app.js
@@ -1,9 +1,18 @@
 let ListGroupItem = () => {
-    return <span>Make this component render like a Bootstrap list group item: <a href="https://getbootstrap.com/docs/4.1/components/list-group/" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    return <ul className="list-unstyled"><li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+    <li className="list-group-item">Morbi leo risus</li>
+    <li className="list-group-item">Porta ac consectetur ac</li>
+    <li className="list-group-item">Vestibulum at eros</li>
+    </ul>
+    
+    // <span>Make this component render like a Bootstrap list group item: <a href="https://getbootstrap.com/docs/4.1/components/list-group/" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
 }
 
 let ListGroup = () => {
-    return <span>Make this Component render a handful of ListGroupItems</span>
+    return <div><ListGroupItem /></div>
+    
+    // <span>Make this Component render a handful of ListGroupItems</span>
 }
 
 
